45dca2e6a68ca0d69e57f7be2c8dcd7f54d07493
Ro706 <60247178+Ro706@users.noreply.github.com>
2023-06-16 12:08:09 +0530

    update

--- alarm.py
+++ alarm.py
@@ -1,16 +1,37 @@
 
 import winsound
+from threading import Thread
 import time
 from time import strftime
 from time import localtime
 import datetime
-while True:
-    time = strftime("%H:%M", localtime())
-    print(time)
-    if time =='12:00':
-        winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
-        break
-    else:
+
+def times():
+    while True:
+        time = strftime("%H:%M", localtime())
+        print(time)
+        if time =='12:07':
+            winsound.PlaySound('alarm.wav', winsound.SND_FILENAME)
+            break
+        else:
+            time.sleep(1)
+            continue
+def printf():
+    n = 0
+    while True:
+        print('hello')
         time.sleep(1)
-        continue
+        n+=1
+        if n == 10:
+            print("Done")
+            break
+        else:
+            continue
+        
+t1 = Thread(target=printf)
+t1.start()
+t2 = Thread(target=times)
+t2.start()
+
+    
 
